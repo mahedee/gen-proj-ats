4b0465be7b2df1848d6fdfcfbbbad4bcf04c9ecf
kajal-alpha <kajal.alpha@gmail.com>
2017-06-19 02:03:02 +0600

    Project Create

---
+++ Batch08/Source/ATS/ATS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ATS.Startup))]
+namespace ATS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
