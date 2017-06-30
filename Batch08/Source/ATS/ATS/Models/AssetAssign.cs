13a561a963229c2a595f7f668b39999a3c8e3522
kajal-alpha <kajal.alpha@gmail.com>
2017-06-30 18:18:00 +0600

    Add some more model class

---
+++ Batch08/Source/ATS/ATS/Models/AssetAssign.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ATS.Models
+{
+    public class AssetAssign
+    {
+        public int Id { get; set; }
+        public string AssetTag { get; set; }
+        public int assignTo { get; set; }
+        public int assignId { get; set; }
+        public virtual AssetRegister Asset { get; set; }
+        public virtual Department Department { get; set; }
+        public virtual Employee Employee { get; set; }
+
+
+        public string CreateBy { get; set; }
+        public string ModifiedBy { get; set; }
+        public string CreateDate { get; set; }
+        public string ActionDate { get; set; }
+    }
+}
